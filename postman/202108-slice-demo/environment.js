f73c69ed7b87a3e97436bef73ee89bff87f2abd0
Roger Steve Ruiz <rogeruiz@truss.works>
2021-09-10 23:18:11 -0400

    Update environment and index entry files

--- postman/202108-slice-demo/environment.js
+++ postman/202108-slice-demo/environment.js
@@ -1,5 +1,11 @@
 const fs = require('fs'),
       path = require('path'),
+      paymentRequestTemplate = fs.readFileSync(
+        path.resolve(
+          __dirname,
+          './templates/prime-update/payment-request.html'
+        )
+      ),
       primeNotificationTemplate = fs.readFileSync(
         path.resolve(
           __dirname,
@@ -12,62 +18,74 @@ const fs = require('fs'),
           './templates/prime-update/counsels-move.html'
         )
       ),
-      DEMO_ENVIRONMENT_URL = 'https://api.demo.dp3.us',
-      ACTUAL_WEIGHT = 5100,
-      ESTIMATED_WEIGHT = 4800;
-
-var environment = {
-  baseUrl: {
-    id: 'baseUrl',
-    value: `${DEMO_ENVIRONMENT_URL}/prime/v1`,
-    type: 'string',
-  },
-  supportUrl: {
-    id: 'supportUrl',
-    value: `${DEMO_ENVIRONMENT_URL}/support/v1`,
-    type: 'string',
-  },
-  reweighID: {
-    id: 'reweighID',
-    value: '',
-    type: 'string',
-  },
-  reweighETag: {
-    id: 'reweighETag',
-    value: '',
-    type: 'string',
-  },
-  shipmentID: {
-    id: 'shipmentID',
-    value: '',
-    type: 'string',
-  },
-  shipmentETag: {
-    id: 'shipmentETag',
-    value: '',
-    type: 'string',
-  },
-  primeEstimatedWeight: {
-    id: 'primeEstimatedWeight',
-    value: ESTIMATED_WEIGHT,
-    type: 'number',
-  },
-  primeActualWeight: {
-    id: 'primeActualWeight',
-    value: ACTUAL_WEIGHT,
-    type: 'number',
-  },
-  primeNotificationTemplate: {
-    id: 'primeNotificationTemplate',
-    value: primeNotificationTemplate.toString(),
-    type: 'string',
-  },
-  updateShipmentTemplate: {
-    id: 'updateShipmentTemplate',
-    value: updateShipmentTemplate.toString(),
-    type: 'string',
-  },
-};
+      api_url = 'https://api.stg.move.mil',
+      environment = {
+        baseUrl: {
+          id: 'baseUrl',
+          value: `${api_url}/prime/v1`,
+          type: 'string',
+        },
+        supportUrl: {
+          id: 'supportUrl',
+          value: `${api_url}/support/v1`,
+          type: 'string',
+        },
+        paymentRequestID: {
+          id: 'paymentRequestID',
+          value: '',
+          type: 'string',
+        },
+        serviceItemsIDs: {
+          id: 'serviceItemsIDs',
+          value: [],
+          type: 'array'
+        },
+        moveTaskOrderID: {
+          id: 'moveTaskOrderID',
+          value: '',
+          type: 'string',
+        },
+        moveTaskOrderETag: {
+          id: 'moveTaskOrderETag',
+          value: '',
+          type: 'string',
+        },
+        reweighID: {
+          id: 'reweighID',
+          value: '',
+          type: 'string',
+        },
+        reweighETag: {
+          id: 'reweighETag',
+          value: '',
+          type: 'string',
+        },
+        shipmentID: {
+          id: 'shipmentID',
+          value: '',
+          type: 'string',
+        },
+        shipmentETag: {
+          id: 'shipmentETag',
+          value: '',
+          type: 'string',
+        },
+        paymentRequestTemplate: {
+          id: 'paymentRequestTemplate',
+          value: paymentRequestTemplate.toString(),
+          type: 'string',
+        },
+        primeNotificationTemplate: {
+          id: 'primeNotificationTemplate',
+          value: primeNotificationTemplate.toString(),
+          type: 'string',
+        },
+        updateShipmentTemplate: {
+          id: 'updateShipmentTemplate',
+          value: updateShipmentTemplate.toString(),
+          type: 'string',
+        },
+      };
 
 module.exports = (function(){
   let environmentList =  [];
--- postman/202108-slice-demo/index.js
+++ postman/202108-slice-demo/index.js
@@ -1,20 +1,32 @@
 const fs = require('fs'),
       path = require('path'),
       Collection = require('postman-collection').Collection,
-      fileName = `${Date.now()}.${path.basename(__dirname + 'postman_collection')}`,
+      fileName = `${Date.now()}.${path.basename(__dirname + '.postman_collection.json')}`,
       savePath = path.join(process.cwd(), '/collections'),
       filePath = `${savePath}/${fileName}`,
       definition = require('./definition'),
-      GetAllMoves = require('./requests/get-all-moves'),
-      NotificationPrimeReceivesMove = require('./requests/notification-prime-receives-move'),
-      UpdateShipmentWithCounseling = require('./requests/update-shipment-with-counseling');
+      GetAllMoves                           = require('./requests/get-all-moves'),
+      NotificationPrimeReceivesMove         = require('./requests/notification-prime-receives-move'),
+      UpdateShipmentWithScheduledPickupDate = require('./requests/update-shipment-with-scheduled-pickup-date'),
+      UpdateShipmentWithEstimatedWeight     = require('./requests/update-shipment-with-estimated-weight'),
+      UpdateShipmentWithActualPickupDate    = require('./requests/update-shipment-with-actual-pickup-date'),
+      UpdateShipmentWithActualWeight        = require('./requests/update-shipment-with-actual-weight'),
+      UpdateShipmentWithReweighWeight       = require('./requests/update-shipment-with-reweigh-weight'),
+      CreatePaymentRequest                  = require('./requests/create-payment-request'),
+      UploadProofOfServiceDocument          = require('./requests/upload-proof-of-service');
 
 var sliceDemoCollection = new Collection({
   ...definition,
   item: [
     GetAllMoves,
     NotificationPrimeReceivesMove,
-    UpdateShipmentWithCounseling,
+    UpdateShipmentWithScheduledPickupDate,
+    UpdateShipmentWithEstimatedWeight,
+    UpdateShipmentWithActualPickupDate,
+    UpdateShipmentWithActualWeight,
+    UpdateShipmentWithReweighWeight,
+    CreatePaymentRequest,
+    UploadProofOfServiceDocument,
   ],
 });
 
